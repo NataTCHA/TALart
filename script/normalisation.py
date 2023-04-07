cc0a545b654a5966625a6fb3f822311076013d36
AnneSophie148 <annesofififi3@gmail.com>
2023-04-07 11:56:30 +0200

    mise à jour normalisation

--- script/normalisation.py
+++ script/normalisation.py
@@ -1,6 +1,18 @@
 def normalisation(mot):
     from unidecode import unidecode
-    mot_normalise = unidecode(mot.capitalize())
+    mot = mot.replace("-", '')
+    mot = mot.replace("l'", '')
+    mot = mot.replace("_", '')
+    mot = mot.replace(".", '')
+    mot = mot.replace("^", '')
+    mot = mot.replace("(", '')
+    mot = mot.replace(")", '')
+    mot = mot.replace("?", '')
+    mot = mot.replace("qu'", '')
+    mot = mot.replace("'", '')
+    mot = mot.replace("d'", '')
+    mot = mot.replace(",", '')
+    mot_normalise = unidecode((mot.replace(",", "")).capitalize())
     return mot_normalise
 
-normalisation(mot)
+normalisation("qu'Apollon")
