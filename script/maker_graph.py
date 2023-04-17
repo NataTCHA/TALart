a28f7fd04ece5754e82d0d2fbd19f0f429ec4452
AnneSophie148 <annesofififi3@gmail.com>
2023-04-17 23:56:56 +0200

    graphique origines

--- script/maker_graph.py
+++ script/maker_graph.py
@@ -36,6 +36,15 @@ def piechart_AC_gender(array_women, array_men):
     plt.legend(title = "Genre dans les Appelations Courtes")
     plt.show() 
 
+def piechart_AC_origin(array_romain, array_grec, array_egyptiens, array_gaulois):
+    import pandas
+    import numpy as np
+    y = np.array([array_romain, array_grec, array_egyptiens, array_gaulois])
+    mylabels = ["Romains", "Grecs", "Egyptiens", "Gaulois"]
+    plt.pie(y, labels = mylabels)
+    plt.legend(title = "Origines des personnages Appelations Courtes")
+    plt.show() 
+
 def dictionnary_gender():
     #cette fonction fait un dictionnaire des personnages et de leur genre
     df = pandas.read_csv("../dictionnaires_personnages/personnage_greco_romain.csv")
@@ -84,4 +93,59 @@ def get_gender():
     print("femmes: ", femmes," hommes:", hommes)
     piechart_AC_gender(femmes, hommes)
 
-get_gender()
+def get_origin():
+    texte = open("../Corpus/AC.txt").readlines()
+    normalized_texte = normalisation(texte, 0)
+    #nb sera le nombre d'occurence pour chaque perso
+    grecs = 0
+    romains = 0
+    egyptiens = 0
+    gaulois = 0
+    origines = dico_origin()
+    for word in normalized_texte:
+        word = ((((word.replace("l'", " ")).replace('/', ' ')).replace('.', ' ')).replace('-', ' ')).replace('?', '')
+        new_list = word.split( )
+        for e in new_list:
+            for cle, valeur in origines.items():
+                for i in range(len(valeur)):
+                    print(valeur[i])
+                    if e == valeur[i] and cle == "romains" or e[1:] == valeur[i] and cle == "romains":
+                        romains += 1
+                    elif e == valeur[i] and cle == "grecs"  or e[1:] == valeur[i] and cle == "grecs":
+                        grecs += 1
+                    elif e == valeur[i] and cle == "egyptiens":
+                        egyptiens += 1
+                    elif e == valeur[i] and cle == "gaulois":
+                        gaulois += 1
+    print("romains: ", romains," grecs:", grecs, " egyptiens", egyptiens, " gaulois:", gaulois)
+    piechart_AC_origin(romains, grecs, egyptiens, gaulois)
+
+def dico_origin():
+    #cette fonction fait un dictionnaire des personnages et de leur genre
+    df = pandas.read_csv("../dictionnaires_personnages/personnage_greco_romain.csv")
+    df=df.astype(str)
+    romain = list(df['Romain'])
+    normal_romain = normalisation(romain, 0)
+    print(normal_romain)
+    grec = list(df['Grec'])
+    normal_grec = normalisation(grec, 0)
+    f = open('../dictionnaires_personnages/gaulois.txt').readlines()
+    f2 = open('../dictionnaires_personnages/egyptiens.txt').readlines()
+    liste_egyptiens = []
+    liste_gaulois = []
+    dico_origin = {}
+    for name in f:
+        name = (name.strip()).split(',')
+        liste_gaulois.append(name[0])
+    for name in f2:
+        name = (name.strip()).split(',')
+        liste_egyptiens.append(name[0])
+    dico_origin["romains"]=normal_romain
+    dico_origin["grecs"]=normal_grec
+    dico_origin["egyptiens"]=liste_egyptiens
+    dico_origin["gaulois"]=liste_gaulois
+    print(dico_origin)
+    return dico_origin
+    
+
+get_origin()
